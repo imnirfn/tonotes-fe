95c7ca14c308b0c0116d96db41370157fbc233f8
s3ns3x <s3ns3xy@yahoo.com>
2020-12-27 17:07:30 +0800

    remove locker repository

--- src/repositories/LockerRepository.js
+++
@@ -1,9 +0,0 @@
-import Repository from './Repository'
-import Locker from './../models/Locker'
-import datasource from '../datasources/baljs-rest-api'
-
-export default class LockerRepository extends Repository {
-  constructor() {
-    super(Locker, datasource)
-  }
-}
